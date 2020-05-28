ed0c2f1f912ec61c863c8456d4d4cdec78c8c39b
Murilo Gomes Teixeira <murilogomes@hotmail.com.br>
2020-05-27 22:55:43 -0300

    Segunda atualização

--- Controller/PainelAdmController.java
+++ Controller/PainelAdmController.java
@@ -23,11 +23,11 @@ public class PainelAdmController implements Initializable {
     public PainelUserController apresentador;
 
     @FXML private Label preferencial_atual;
-    private int p_atual;
+    private int p_atual = 0;
     @FXML private Label preferencial_anterior;
-    private int p_anterior;
+    private int p_anterior = 0;
     @FXML private Label preferencial_seguinte;
-    private int p_seguinte;
+    private int p_seguinte = 1;
     @FXML private Label comum_atual;
     private int c_atual = 0;
     @FXML private Label comum_anterior;
@@ -37,15 +37,18 @@ public class PainelAdmController implements Initializable {
 
     @FXML public void avancar_comum(ActionEvent event) {
         if(c_atual == 0){
-            System.out.println("no if");
             c_atual = c_atual+1;
             System.out.println(c_atual);
             c_seguinte = c_seguinte+1;
             apresentador.senha_atual.setText(String.valueOf(c_atual));
             comum_atual.setText(String.valueOf(c_atual));
             comum_seguinte.setText(String.valueOf(c_seguinte));
+            apresentador.senha_anterior5.setText(apresentador.senha_anterior4.getText());
+            apresentador.senha_anterior4.setText(apresentador.senha_anterior3.getText());
+            apresentador.senha_anterior3.setText(apresentador.senha_anterior2.getText());
+            apresentador.senha_anterior2.setText(apresentador.senha_anterior1.getText());
+            apresentador.senha_anterior1.setText(apresentador.senha_atual.getText());
         }else{
-            System.out.println("fora do if");
             c_anterior = c_atual;
             c_atual = c_atual+1;
             c_seguinte = c_atual+1;
@@ -62,15 +65,69 @@ public class PainelAdmController implements Initializable {
     }
 
     @FXML public void avancar_preferencial(ActionEvent event) {
-
+        if(p_atual == 0){
+            p_atual = p_atual+1;
+            p_seguinte = p_seguinte+1;
+            apresentador.senha_atual.setText("P-"+String.valueOf(p_atual));
+            preferencial_atual.setText("P-"+String.valueOf(p_atual));
+            preferencial_seguinte.setText("P-"+String.valueOf(p_seguinte));
+            apresentador.senha_anterior5.setText(apresentador.senha_anterior4.getText());
+            apresentador.senha_anterior4.setText(apresentador.senha_anterior3.getText());
+            apresentador.senha_anterior3.setText(apresentador.senha_anterior2.getText());
+            apresentador.senha_anterior2.setText(apresentador.senha_anterior1.getText());
+            apresentador.senha_anterior1.setText(apresentador.senha_atual.getText());
+        }else{
+            p_anterior = p_atual;
+            p_atual = p_atual+1;
+            p_seguinte = p_atual+1;
+            apresentador.senha_anterior5.setText(apresentador.senha_anterior4.getText());
+            apresentador.senha_anterior4.setText(apresentador.senha_anterior3.getText());
+            apresentador.senha_anterior3.setText(apresentador.senha_anterior2.getText());
+            apresentador.senha_anterior2.setText(apresentador.senha_anterior1.getText());
+            apresentador.senha_anterior1.setText(apresentador.senha_atual.getText());
+            apresentador.senha_atual.setText("P-"+String.valueOf(p_atual));
+            preferencial_anterior.setText("P-"+String.valueOf(p_anterior));
+            preferencial_atual.setText("P-"+String.valueOf(p_atual));
+            preferencial_seguinte.setText("P-"+String.valueOf(p_seguinte));
+        }
     }
 
     @FXML public void voltar_comum(ActionEvent event) {
-
+        if(c_atual == 0){
+            System.out.println("nao pode");
+        }else{
+             c_seguinte = c_atual;
+             c_atual = c_anterior;
+             c_anterior = c_anterior-1;
+            apresentador.senha_anterior5.setText(apresentador.senha_anterior4.getText());
+            apresentador.senha_anterior4.setText(apresentador.senha_anterior3.getText());
+            apresentador.senha_anterior3.setText(apresentador.senha_anterior2.getText());
+            apresentador.senha_anterior2.setText(apresentador.senha_anterior1.getText());
+            apresentador.senha_anterior1.setText(apresentador.senha_atual.getText());
+            apresentador.senha_atual.setText(String.valueOf(c_atual));
+            comum_anterior.setText(String.valueOf(c_anterior));
+            comum_atual.setText(String.valueOf(c_atual));
+            comum_seguinte.setText(String.valueOf(c_seguinte));
+        }
     }
 
     @FXML public void voltar_preferencial(ActionEvent event) {
-
+        if(p_atual == 0){
+            System.out.println("nao pode");
+        }else{
+            p_seguinte = p_atual;
+            p_atual = p_anterior;
+            p_anterior = p_anterior-1;
+            apresentador.senha_anterior5.setText(apresentador.senha_anterior4.getText());
+            apresentador.senha_anterior4.setText(apresentador.senha_anterior3.getText());
+            apresentador.senha_anterior3.setText(apresentador.senha_anterior2.getText());
+            apresentador.senha_anterior2.setText(apresentador.senha_anterior1.getText());
+            apresentador.senha_anterior1.setText(apresentador.senha_atual.getText());
+            apresentador.senha_atual.setText(String.valueOf(p_atual));
+            preferencial_anterior.setText(String.valueOf(p_anterior));
+            preferencial_atual.setText(String.valueOf(p_atual));
+            preferencial_seguinte.setText(String.valueOf(p_seguinte));
+        }
     }
 
     @Override
